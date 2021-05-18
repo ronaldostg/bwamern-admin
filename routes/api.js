06bdc3212989891419d9d814fc6cb0e3a4cfd9f0
ronaldostg <sitanggang761@gmail.com>
2021-05-18 15:11:27 +0700

    upload first project

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const apiController = require('../controllers/apiController')
+const {uploadSingle} = require('../middlewares/multer')
+
+
+// ======================== endpoioint for login 
+router.get('/landing-page', apiController.landingPage); 
+router.get('/detail-page/:id', apiController.detailPage); 
+router.post('/booking-page', uploadSingle, apiController.bookingPage); 
+
+
+module.exports = router;
